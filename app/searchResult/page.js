02517e03fc10cb968bef9150390f6ce7174f1e16
Xeeshan <xeeshann.ahsan@gmail.com>
2025-02-10 02:03:14 +0600

    suspense search page

--- app/searchResult/page.js
+++ app/searchResult/page.js
@@ -1,7 +1,13 @@
+"use client";
 import Search from "@/components/Movie/Search";
+import { Suspense } from "react";
 
 const SearchPage = () => {
-  return <Search />;
+  return (
+    <Suspense fallback={<div>Loading search results...</div>}>
+      <Search />
+    </Suspense>
+  );
 };
 
 export default SearchPage;
